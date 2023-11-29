79983912e3bbf56e72b4f80ff6e53d1302e5a175
ghostcode-cl <152226729+ghostcode-cl@users.noreply.github.com>
2023-11-29 11:39:00 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,40 @@
+﻿# MonitoreoSaludCMV
+
+This Flask application is designed to collect health-related survey data from patients and store it in a MongoDB collection. The survey aims to gather feedback regarding health services in Valparaíso. Here is a brief explanation of each segment:
+
+1. **Environment Variables**: The app begins by loading `.env.development` file where sensitive information is stored like the `SECRET_KEY` for Flask and the MongoDB connection URI.
+
+2. **Flask Configuration**: The application is configured with a secret key fetched from the environment variables for session management.
+
+3. **MongoDB Setup**: It establishes a connection to MongoDB using the `MONGODB_URI`. It defines which database and collection to use (`cmvalparaisoDas` database and `opinionSaludValparaiso` collection).
+
+4. **Rate Limiting**: To prevent abuse, a rate limiter is applied using `RATE_LIMITER_URI`, restricting users to 60 requests per 30 minutes.
+
+5. **Content Security Policy**: A CSP header is added to all responses for additional security.
+
+6. **Home Route**: The root route (`/`) handles both GET and POST requests for the survey form.
+   - On POST, it validates and extracts data from the form fields, such as age, gender, health center, visit frequency, satisfaction level, recommendation level, and reasons for the given ratings.
+   - The data is inserted into the MongoDB collection with the current date and a static target value.
+   - After successful submission, the user is redirected to a thank-you page.
+
+7. **Error Handling**: Custom error handlers are set up for 404 (Not Found), 500 (Internal Server Error), and 429 (Rate Limit Exceeded).
+
+8. **Running the App**: At the end, the application is set to run in debug mode.
+
+### HTML Template Overview:
+
+The HTML template renders a survey form with various input fields:
+
+1. **Basic Setup**: It sets the document type, language, character set, and viewport settings. It includes a title and a link to the CSS stylesheet.
+
+2. **Survey Form**:
+   - It includes fields for age, gender, affiliated health center (CESFAM), frequency of visits, satisfaction level, and likelihood of recommending the center.
+   - Satisfaction and recommendation levels use a visual scale with interactive elements (though the actual JavaScript functions `highlightNumber()` are not provided here).
+   - Users can provide a reason for their ratings in a text area.
+   - A character count for the text area is also displayed.
+
+3. **Submission**: The form is submitted via POST to the home route for processing.
+
+4. **FAQ Link**: An accessible link to a FAQ page is provided, featuring a question mark icon and a decorative arrow.
+
+5. **Scripts**: The template links to an external JavaScript file, presumably for form interactions and validation.
---
+++ app.py
@@ -0,0 +1,128 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+from werkzeug.exceptions import BadRequest
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask import Flask, render_template_string, request
+
+# Load environment variables from the .env.development file
+# Cargar variables de entorno del archivo .env.development
+load_dotenv(".env.development")
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+# MongoDB configuration
+# Configuración de MongoDB
+uri = os.getenv('MONGODB_URI')
+client = MongoClient(uri)
+db = client['cmvalparaisoDas']
+collection = db['opinionSaludValparaiso']
+
+# MongoDB URI from the .env for Flask rate limiter
+# URI de MongoDB desde el .env para el limitador de tasa de Flask
+#rate_limiter_db_uri = os.getenv('RATE_LIMITER_URI')
+
+# Defining the rate limiter to limit requests per IP
+# Definiendo el limitador de tasa para limitar solicitudes por IP
+"""
+limiter = Limiter(
+    app=app,
+    key_func=get_remote_address,
+    storage_uri=rate_limiter_db_uri,
+    strategy='fixed-window'
+)
+"""
+
+# Apply Content-Security-Policy (CSP) to each response
+# Aplica la Política de Seguridad de Contenido (CSP) a cada respuesta
+@app.after_request
+def apply_csp(response):
+    response.headers['Content-Security-Policy'] = "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;"
+    return response
+
+# Home route
+# Ruta de inicio
+@app.route('/', methods=['GET', 'POST'])
+#@limiter.limit("60 per 30 minute")
+def home():
+    if request.method == 'POST':
+        try:
+            # Extract data from the form and validate
+            # Extraer datos del formulario y validar
+            edad = int(request.form.get('edad'))  # Convert age to integer and validate
+            genero = request.form.get('genero')
+            cesfam = request.form.get('cesfam')
+            frecuencia = request.form.get('frecuencia')
+            satisfaccion = int(request.form.get('satisfaccion'))  # Convert satisfaction to integer and validate
+            recomendacion = int(request.form.get('recomendacion'))  # Convert recommendation to integer and validate
+            razon = request.form.get('razon')
+            target = 3
+
+            # Get the current date and time
+            # Obtener la fecha y hora actuales
+            date = datetime.now()
+
+            # Check for empty fields
+            # Verificar si hay campos vacíos
+            if not (edad and genero and cesfam and frecuencia and razon):
+                raise BadRequest("Todos los campos deben ser rellenados.")
+
+            # Insert data into MongoDB
+            # Insertar datos en MongoDB
+            collection.insert_one({
+                'edad': edad,
+                'genero': genero,
+                'cesfam': cesfam,
+                'frecuencia': frecuencia,
+                'satisfaccion': satisfaccion,
+                'recomendacion': recomendacion,
+                'razon': razon,
+                'date': date,
+                'target': target
+            })
+
+            return redirect(url_for('thank_you'))
+        except (BadRequest, ValueError) as e:  # Error handling for data conversion or insertion
+            error = str(e)
+            return render_template('index.html', error=error)
+        
+    return render_template('index.html')
+
+# Frequently Asked Questions route
+# Ruta de Preguntas Frecuentes
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+# Thank you page after submission
+# Página de agradecimiento después de enviar
+@app.route('/thankyou')
+def thank_you():
+    return render_template('thankyou.html')
+
+# Route for CSS styles
+# Ruta para estilos CSS
+@app.route('/styles.css')
+def css():
+    return redirect(url_for('static', filename='styles.css'))
+
+# Error handlers
+# Manejadores de errores
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.errorhandler(429)
+def ratelimit_error(e):
+    return render_template_string("Haz alcanzado el límite de solicitudes"), 429
+
+if __name__ == "__main__":
+    app.run(debug=True)
